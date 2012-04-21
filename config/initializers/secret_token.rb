fee44339a7af118bfcc38e5c8f805cb6927fed74
Apoorva <amehta@Apoorvas-MacBook-Air.local>
2012-04-21 15:03:01 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maplecrew::Application.config.secret_token = '75b9487fb59035c2a8c6ae9aba514637b2e4cce2ee10d9ec89dfc741e465574ba9d19940984a667ff2b9b1b9723d3d92d13d8133c2b5a230416a72f0b35141b6'
