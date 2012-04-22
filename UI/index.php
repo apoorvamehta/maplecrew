769e759f7328771ba3cb642e50c7409f863eb55d
Adil Lalani <alalani@twitvid.com>
2012-04-21 17:47:44 -0700

    adding includes

--- UI/index.php
+++ UI/index.php
@@ -1,5 +1,9 @@
 <?php
 
+//Includes
+require_once 'libs/Mustache.php';
+require_once 'libs/MustacheLoader.php';
+
 //Globals
 $m = new Mustache();
 $ml = new MustacheLoader(dirname(__FILE__).'/templates');
