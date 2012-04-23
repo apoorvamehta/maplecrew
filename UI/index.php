67e5a28b452f2a8e801dec38a649d16676dd7447
Adil Lalani <alalani@twitvid.com>
2012-04-22 21:00:40 -0700

    add interests

--- UI/index.php
+++ UI/index.php
@@ -29,8 +29,9 @@ switch($_REQUEST['section']) {
 
 	case "add_interests":
 		$data = callAPI("female_friends", array());
+		$c['interests'] = array();
 		foreach($data as $i) $c['interests'][] = json_decode($i, true);
-		//var_dump($c['interests']);
+		var_dump($c['interests']);
 		//exit;
 		echo $m->render($ml["header"], array());
 		//echo $m->render($ml["top"], array());
