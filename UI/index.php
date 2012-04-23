a9ba3128e54e02aaba1c059a14269b4a623caa25
Adil Lalani <alalani@twitvid.com>
2012-04-22 17:12:55 -0700

    adding feed call

--- UI/index.php
+++ UI/index.php
@@ -3,8 +3,10 @@
 //Includes
 require_once 'libs/Mustache.php';
 require_once 'libs/MustacheLoader.php';
+require_once 'libs/functions.php';
 
 //Globals
+define("API_PATH", "http://localhost:3000/api/");
 $m = new Mustache();
 $ml = new MustacheLoader(dirname(__FILE__).'/templates');
 
@@ -12,6 +14,10 @@ $ml = new MustacheLoader(dirname(__FILE__).'/templates');
 $data[0] = array("img"=>"http://photos-h.ak.fbcdn.net/hphotos-ak-ash3/556524_10101142755271083_1229271_63317478_922457948_a.jpg", "img_big"=>"http://photos-h.ak.fbcdn.net/hphotos-ak-ash3/s720x720/556524_10101142755271083_1229271_63317478_922457948_n.jpg", "name"=>"Tonya", "caption"=>"test 123...");
 $data[1] = array("img"=>"http://photos-a.ak.fbcdn.net/hphotos-ak-ash3/561876_10101142759208193_1229271_63317497_829744965_a.jpg", "img_big"=>"http://photos-a.ak.fbcdn.net/hphotos-ak-ash3/s720x720/561876_10101142759208193_1229271_63317497_829744965_n.jpg", "name"=>"Tonya", "caption"=>"test 123...");
 for($i=0;$i<100;$i++) $c['interests'][] = $data[rand(0,1)];
+
+if($_REQUEST['use_api']) {
+	$c['interests'] = callAPI("board", array("limit"=>100, "index"=>0));
+}
 
 
 echo $m->render($ml["header"], array());
---
+++ UI/libs/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function callAPI($call, $data) {
+	
+	$call = API_PATH.$call."?access_token=".$_COOKIE['access_token'];
+	
+	foreach($data as $k=>$v) $call .= $k."=".$v."&";
+	
+	$res = file_get_contents($call);
+	$res = json_decode($res);
+	
+	return $res;
+}
+
+?>
